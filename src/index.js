d7fbd3423e77d57eb74c6b782357ef9995660e8e
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-11 09:47:41 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './assets/styles/index.scss';
-import App from './App';
 import { ApiContext } from './context/ApiContext';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <ApiContext.Provider value="https://restapi.fr/api/recipes">
-      <App />
-    </ApiContext.Provider>
+    <RouterProvider router={router}>
+      <ApiContext.Provider value="https://restapi.fr/api/recipes" />
+    </RouterProvider>
   </StrictMode>
 );
